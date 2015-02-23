1e69dd95adfde328be861d6a6f7b91c97a523832
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-02-23 16:47:47 +0800

    First commit

---
+++ src/main/java/tw/kid7/BannerMaker.java
@@ -0,0 +1,16 @@
+package tw.kid7;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BannerMaker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
