a4e10cd08e267ff6cc12002d899dd3669bdf7f9a
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-02-24 19:24:30 +0800

    Add IOUtil

---
+++ src/main/java/tw/kid7/BannerMaker/util/IOUtil.java
@@ -0,0 +1,20 @@
+package tw.kid7.BannerMaker.util;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class IOUtil {
+
+    //儲存旗幟
+    static public void saveBanner(Player player, ItemStack banner) {
+        //TODO
+    }
+
+    //讀取旗幟
+    static public ItemStack[] loadBanner(Player player) {
+        ItemStack[] bannerList = new ItemStack[]{};
+        //TODO
+
+        return bannerList;
+    }
+}
