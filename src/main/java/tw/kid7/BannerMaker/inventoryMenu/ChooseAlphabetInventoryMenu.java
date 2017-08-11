0f6326dc47018ce7e8fa0a178afdda6958604cea
KID <jyhsu2000@yahoo.com.tw>
2017-08-12 01:31:45 +0800

    Use new Clickable system on ChooseAlphabetInventoryMenu

--- src/main/java/tw/kid7/BannerMaker/inventoryMenu/ChooseAlphabetInventoryMenu.java
+++ src/main/java/tw/kid7/BannerMaker/inventoryMenu/ChooseAlphabetInventoryMenu.java
@@ -5,7 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.block.banner.Pattern;
 import org.bukkit.block.banner.PatternType;
 import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BannerMeta;
@@ -21,9 +21,6 @@ import static tw.kid7.BannerMaker.configuration.Language.tl;
 
 public class ChooseAlphabetInventoryMenu extends AbstractInventoryMenu {
     private static ChooseAlphabetInventoryMenu instance = null;
-    //按鈕位置
-    private final int buttonPositionBackToMenu = 45;
-    private final int buttonPositionToggleBorder = 49;
 
     public static ChooseAlphabetInventoryMenu getInstance() {
         if (instance == null) {
@@ -33,10 +30,10 @@ public class ChooseAlphabetInventoryMenu extends AbstractInventoryMenu {
     }
 
     @Override
-    public void open(Player player) {
-        PlayerData playerData = BannerMaker.getInstance().playerDataMap.get(player);
+    public void open(final Player player) {
+        final PlayerData playerData = BannerMaker.getInstance().playerDataMap.get(player);
         //建立選單
-        Inventory menu = InventoryMenuUtil.create(tl("gui.alphabet-and-number"));
+        Inventory menu = createClickableMenu(player, tl("gui.alphabet-and-number"));
         //清除當前編輯中的字母
         playerData.setCurrentAlphabetBanner(null);
         //邊框切換按鈕
@@ -51,46 +48,35 @@ public class ChooseAlphabetInventoryMenu extends AbstractInventoryMenu {
         char[] alphabetArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789?!.".toCharArray();
         for (int i = 0; i < alphabetArray.length && i < 54; i++) {
             char alphabet = alphabetArray[i];
-            ItemStack alphabetItem = AlphabetBanner.get(String.valueOf(alphabet), DyeColor.WHITE, DyeColor.BLACK, alphabetBorder);
-            menu.setItem(i, alphabetItem);
+            final AlphabetBanner alphabetBanner = new AlphabetBanner(String.valueOf(alphabet), DyeColor.WHITE, DyeColor.BLACK, alphabetBorder);
+            ItemStack alphabetItem = alphabetBanner.toItemStack();
+            setClickableItem(menu, i, alphabetItem, ClickType.LEFT, new Clickable() {
+                @Override
+                public void action() {
+                    //設定當前編輯中的字母
+                    playerData.setCurrentAlphabetBanner(alphabetBanner);
+                    InventoryMenuUtil.openMenu(player, InventoryMenuState.CREATE_ALPHABET);
+                }
+            });
         }
         //切換有無邊框
-        menu.setItem(buttonPositionToggleBorder, btnBorderedBanner);
+        setClickableItem(menu, 49, btnBorderedBanner, ClickType.LEFT, new Clickable() {
+            @Override
+            public void action() {
+                playerData.setAlphabetBannerBordered(!playerData.isAlphabetBannerBordered());
+                InventoryMenuUtil.openMenu(player);
+            }
+        });
 
         //返回
         ItemStack btnBackToMenu = new ItemBuilder(Material.WOOL).amount(1).durability(14).name(MessageUtil.format("&c" + tl("gui.back"))).build();
-        menu.setItem(buttonPositionBackToMenu, btnBackToMenu);
+        setClickableItem(menu, 45, btnBackToMenu, ClickType.LEFT, new Clickable() {
+            @Override
+            public void action() {
+                InventoryMenuUtil.openMenu(player, InventoryMenuState.MAIN_MENU);
+            }
+        });
         //開啟選單
         player.openInventory(menu);
-    }
-
-    @Override
-    public void onClick(InventoryClickEvent event) {
-        Player player = (Player) event.getWhoClicked();
-        PlayerData playerData = BannerMaker.getInstance().playerDataMap.get(player);
-        ItemStack itemStack = event.getCurrentItem();
-        int rawSlot = event.getRawSlot();
-
-        if (rawSlot < 45) {
-            //選擇字母
-            boolean alphabetBorder = playerData.isAlphabetBannerBordered();
-            //設定當前編輯中的字母
-            AlphabetBanner currentAlphabetBanner = new AlphabetBanner(itemStack.getItemMeta().getDisplayName(), DyeColor.WHITE, DyeColor.BLACK, alphabetBorder);
-            playerData.setCurrentAlphabetBanner(currentAlphabetBanner);
-            InventoryMenuUtil.openMenu(player, InventoryMenuState.CREATE_ALPHABET);
-            return;
-        }
-        //點擊按鈕
-        if (rawSlot == buttonPositionToggleBorder) {
-            //切換有無邊框
-            playerData.setAlphabetBannerBordered(!playerData.isAlphabetBannerBordered());
-            InventoryMenuUtil.openMenu(player);
-            return;
-        }
-        if (rawSlot == buttonPositionBackToMenu) {
-            InventoryMenuUtil.openMenu(player, InventoryMenuState.MAIN_MENU);
-            return;
-        }
-
     }
 }
