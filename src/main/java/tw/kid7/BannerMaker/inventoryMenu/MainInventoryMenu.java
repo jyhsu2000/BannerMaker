baf26f101c36b75855d7d33c814dba2e4e3d1214
KID <jyhsu2000@yahoo.com.tw>
2017-08-12 00:30:13 +0800

    Use new Clickable system on MainInventoryMenu

--- src/main/java/tw/kid7/BannerMaker/inventoryMenu/MainInventoryMenu.java
+++ src/main/java/tw/kid7/BannerMaker/inventoryMenu/MainInventoryMenu.java
@@ -2,7 +2,7 @@ package tw.kid7.BannerMaker.inventoryMenu;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -17,11 +17,6 @@ import static tw.kid7.BannerMaker.configuration.Language.tl;
 
 public class MainInventoryMenu extends AbstractInventoryMenu {
     private static MainInventoryMenu instance = null;
-    //按鈕位置
-    private final int buttonPositionPrevPage = 45;
-    private final int buttonPositionNextPage = 53;
-    private final int buttonPositionCreateBanner = 49;
-    private final int buttonPositionCreateAlphabet = 51;
 
     public static MainInventoryMenu getInstance() {
         if (instance == null) {
@@ -31,17 +26,23 @@ public class MainInventoryMenu extends AbstractInventoryMenu {
     }
 
     @Override
-    public void open(Player player) {
-        PlayerData playerData = BannerMaker.getInstance().playerDataMap.get(player);
+    public void open(final Player player) {
+        final PlayerData playerData = BannerMaker.getInstance().playerDataMap.get(player);
         //建立選單
         Inventory menu = InventoryMenuUtil.create(tl("gui.main-menu"));
         //當前頁數
-        int currentPage = playerData.getCurrentPage();
+        final int currentPage = playerData.getCurrentPage();
         //顯示現有旗幟
         List<ItemStack> bannerList = IOUtil.loadBannerList(player, currentPage);
         for (int i = 0; i < bannerList.size() && i < 45; i++) {
-            ItemStack banner = bannerList.get(i);
+            final ItemStack banner = bannerList.get(i);
             menu.setItem(i, banner);
+            setClickableItem(menu, i, banner, ClickType.LEFT, new Clickable() {
+                @Override
+                public void action() {
+                    InventoryMenuUtil.showBannerInfo(player, banner);
+                }
+            });
         }
         //總頁數
         int totalPage = (int) Math.ceil(IOUtil.getBannerCount(player) / 45.0);
@@ -50,62 +51,49 @@ public class MainInventoryMenu extends AbstractInventoryMenu {
         //上一頁
         if (currentPage > 1) {
             ItemStack prevPage = new ItemBuilder(Material.ARROW).amount(currentPage - 1).name(MessageUtil.format("&a" + tl("gui.prev-page"))).build();
-            menu.setItem(buttonPositionPrevPage, prevPage);
+            setClickableItem(menu, 45, prevPage, ClickType.LEFT, new Clickable() {
+                @Override
+                public void action() {
+                    playerData.setCurrentPage(currentPage - 1);
+                    InventoryMenuUtil.openMenu(player);
+                }
+            });
         }
         //下一頁
         if (currentPage < totalPage) {
             ItemStack nextPage = new ItemBuilder(Material.ARROW).amount(currentPage + 1).name(MessageUtil.format("&a" + tl("gui.next-page"))).build();
-            menu.setItem(buttonPositionNextPage, nextPage);
+            setClickableItem(menu, 53, nextPage, ClickType.LEFT, new Clickable() {
+                @Override
+                public void action() {
+                    playerData.setCurrentPage(currentPage + 1);
+                    InventoryMenuUtil.openMenu(player);
+                }
+            });
         }
         //Create banner
         ItemStack btnCreateBanner = new ItemBuilder(Material.WOOL).amount(1).durability(5).name(MessageUtil.format("&a" + tl("gui.create-banner"))).build();
-        menu.setItem(buttonPositionCreateBanner, btnCreateBanner);
+        setClickableItem(menu, 49, btnCreateBanner, ClickType.LEFT, new Clickable() {
+            @Override
+            public void action() {
+                InventoryMenuUtil.openMenu(player, InventoryMenuState.CREATE_BANNER);
+            }
+        });
         //建立字母
         if (BannerMaker.getInstance().enableAlphabetAndNumber) {
             ItemStack btnCreateAlphabet = AlphabetBanner.get("A");
             ItemMeta btnCreateAlphabetItemMeta = btnCreateAlphabet.getItemMeta();
             btnCreateAlphabetItemMeta.setDisplayName(MessageUtil.format("&a" + tl("gui.alphabet-and-number")));
             btnCreateAlphabet.setItemMeta(btnCreateAlphabetItemMeta);
-            menu.setItem(buttonPositionCreateAlphabet, btnCreateAlphabet);
+            setClickableItem(menu, 51, btnCreateAlphabet, ClickType.LEFT, new Clickable() {
+                @Override
+                public void action() {
+                    if (BannerMaker.getInstance().enableAlphabetAndNumber) {
+                        InventoryMenuUtil.openMenu(player, InventoryMenuState.CHOOSE_ALPHABET);
+                    }
+                }
+            });
         }
         //開啟選單
         player.openInventory(menu);
-    }
-
-    @Override
-    public void onClick(InventoryClickEvent event) {
-        Player player = (Player) event.getWhoClicked();
-        PlayerData playerData = BannerMaker.getInstance().playerDataMap.get(player);
-        ItemStack itemStack = event.getCurrentItem();
-        int rawSlot = event.getRawSlot();
-        if (rawSlot < 45) {
-            //點擊旗幟
-            //顯示旗幟
-            InventoryMenuUtil.showBannerInfo(player, itemStack);
-            return;
-        }
-        //當前頁數
-        int currentPage = playerData.getCurrentPage();
-        //修改狀態
-        if (rawSlot == buttonPositionPrevPage) {
-            playerData.setCurrentPage(currentPage - 1);
-            InventoryMenuUtil.openMenu(player);
-            return;
-        }
-        if (rawSlot == buttonPositionNextPage) {
-            playerData.setCurrentPage(currentPage + 1);
-            InventoryMenuUtil.openMenu(player);
-            return;
-        }
-        if (rawSlot == buttonPositionCreateBanner) {
-            InventoryMenuUtil.openMenu(player, InventoryMenuState.CREATE_BANNER);
-            return;
-        }
-        if (rawSlot == buttonPositionCreateAlphabet) {
-            if (BannerMaker.getInstance().enableAlphabetAndNumber) {
-                InventoryMenuUtil.openMenu(player, InventoryMenuState.CHOOSE_ALPHABET);
-            }
-            return;
-        }
     }
 }
