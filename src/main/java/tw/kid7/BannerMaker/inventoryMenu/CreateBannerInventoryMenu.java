4fef2a6f5a8ec03880372022d0c2e7958554d85b
KID <jyhsu2000@yahoo.com.tw>
2017-08-12 01:49:20 +0800

    Use new Clickable system on CreateBannerInventoryMenu

--- src/main/java/tw/kid7/BannerMaker/inventoryMenu/CreateBannerInventoryMenu.java
+++ src/main/java/tw/kid7/BannerMaker/inventoryMenu/CreateBannerInventoryMenu.java
@@ -5,7 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.block.banner.Pattern;
 import org.bukkit.block.banner.PatternType;
 import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BannerMeta;
@@ -18,12 +18,6 @@ import static tw.kid7.BannerMaker.configuration.Language.tl;
 
 public class CreateBannerInventoryMenu extends AbstractInventoryMenu {
     private static CreateBannerInventoryMenu instance = null;
-    //按鈕位置
-    private final int buttonPositionMorePattern = 51;
-    private final int buttonPositionBackToMenu = 45;
-    private final int buttonPositionCreate = 53;
-    private final int buttonPositionDelete = 47;
-    private final int buttonPositionRemovePattern = 49;
 
     public static CreateBannerInventoryMenu getInstance() {
         if (instance == null) {
@@ -33,17 +27,23 @@ public class CreateBannerInventoryMenu extends AbstractInventoryMenu {
     }
 
     @Override
-    public void open(Player player) {
-        PlayerData playerData = BannerMaker.getInstance().playerDataMap.get(player);
+    public void open(final Player player) {
+        final PlayerData playerData = BannerMaker.getInstance().playerDataMap.get(player);
         //建立選單
-        Inventory menu = InventoryMenuUtil.create(tl("gui.create-banner"));
+        Inventory menu = createClickableMenu(player, tl("gui.create-banner"));
         //取得當前編輯中的旗幟
-        ItemStack currentBanner = playerData.getCurrentEditBanner();
+        final ItemStack currentBanner = playerData.getCurrentEditBanner();
         if (currentBanner == null) {
             //剛開始編輯，先選擇底色
             for (int i = 0; i < 16; i++) {
-                ItemStack banner = new ItemStack(Material.BANNER, 1, (short) i);
-                menu.setItem(i + 1 + (i / 8), banner);
+                final ItemStack banner = new ItemStack(Material.BANNER, 1, (short) i);
+                setClickableItem(menu, i + 1 + (i / 8), banner, ClickType.LEFT, new Clickable() {
+                    @Override
+                    public void action() {
+                        playerData.setCurrentEditBanner(banner);
+                        InventoryMenuUtil.openMenu(player);
+                    }
+                });
             }
         } else {
             //新增按鈕
@@ -57,8 +57,14 @@ public class CreateBannerInventoryMenu extends AbstractInventoryMenu {
             }
             //顏色
             for (int i = 0; i < 16; i++) {
-                ItemStack dye = new ItemBuilder(Material.INK_SACK).amount(1).durability(i).build();
-                menu.setItem(i + 1 + (i / 8), dye);
+                final ItemStack dye = new ItemBuilder(Material.INK_SACK).amount(1).durability(i).build();
+                setClickableItem(menu, i + 1 + (i / 8), dye, ClickType.LEFT, new Clickable() {
+                    @Override
+                    public void action() {
+                        playerData.setSelectedColor(DyeColorUtil.fromInt(dye.getDurability()));
+                        InventoryMenuUtil.openMenu(player);
+                    }
+                });
             }
             //Pattern
             //選擇的顏色
@@ -72,101 +78,80 @@ public class CreateBannerInventoryMenu extends AbstractInventoryMenu {
                     break;
                 }
                 //預覽旗幟
-                ItemStack banner = new ItemStack(Material.BANNER, 1, currentBanner.getDurability());
-                BannerMeta bm = (BannerMeta) banner.getItemMeta();
+                final ItemStack banner = new ItemStack(Material.BANNER, 1, currentBanner.getDurability());
+                final BannerMeta bm = (BannerMeta) banner.getItemMeta();
                 PatternType patternType = BannerUtil.getPatternTypeList().get(patternIndex);
                 bm.addPattern(new Pattern(selectedColor, patternType));
                 banner.setItemMeta(bm);
 
-                menu.setItem(i + 19 + (i / 8), banner);
+                setClickableItem(menu, i + 19 + (i / 8), banner, ClickType.LEFT, new Clickable() {
+                    @Override
+                    public void action() {
+                        //新增Pattern
+                        BannerMeta bm = (BannerMeta) banner.getItemMeta();
+                        Pattern pattern = bm.getPattern(bm.numberOfPatterns() - 1);
+                        BannerMeta currentBm = (BannerMeta) currentBanner.getItemMeta();
+                        currentBm.addPattern(pattern);
+                        currentBanner.setItemMeta(currentBm);
+                        playerData.setCurrentEditBanner(currentBanner);
+                        InventoryMenuUtil.openMenu(player);
+                    }
+                });
             }
             //更多Pattern
             ItemStack btnMorePattern = new ItemBuilder(Material.NETHER_STAR).amount(1).name(MessageUtil.format("&a" + tl("gui.more-patterns"))).build();
-            menu.setItem(buttonPositionMorePattern, btnMorePattern);
+            setClickableItem(menu, 51, btnMorePattern, ClickType.LEFT, new Clickable() {
+                @Override
+                public void action() {
+                    playerData.setShowMorePatterns(!playerData.isShowMorePatterns());
+                    InventoryMenuUtil.openMenu(player);
+                }
+            });
         }
         //返回
         ItemStack btnBackToMenu = new ItemBuilder(Material.WOOL).amount(1).durability(14).name(MessageUtil.format("&c" + tl("gui.back"))).build();
-        menu.setItem(buttonPositionBackToMenu, btnBackToMenu);
+        setClickableItem(menu, 45, btnBackToMenu, ClickType.LEFT, new Clickable() {
+            @Override
+            public void action() {
+                InventoryMenuUtil.openMenu(player, InventoryMenuState.MAIN_MENU);
+            }
+        });
         if (currentBanner != null) {
             //建立旗幟
             ItemStack btnCreate = new ItemBuilder(Material.WOOL).amount(1).durability(5).name(MessageUtil.format("&a" + tl("gui.create"))).build();
-            menu.setItem(buttonPositionCreate, btnCreate);
+            setClickableItem(menu, 53, btnCreate, ClickType.LEFT, new Clickable() {
+                @Override
+                public void action() {
+                    IOUtil.saveBanner(player, currentBanner);
+                    playerData.setCurrentEditBanner(null);
+                    InventoryMenuUtil.openMenu(player, InventoryMenuState.MAIN_MENU);
+                }
+            });
             //刪除
             ItemStack btnDelete = new ItemBuilder(Material.BARRIER).amount(1).name(MessageUtil.format("&c" + tl("gui.delete"))).build();
-            menu.setItem(buttonPositionDelete, btnDelete);
+            setClickableItem(menu, 47, btnDelete, ClickType.LEFT, new Clickable() {
+                @Override
+                public void action() {
+                    playerData.setCurrentEditBanner(null);
+                    InventoryMenuUtil.openMenu(player);
+                }
+            });
             if (currentBanner.hasItemMeta() && ((BannerMeta) currentBanner.getItemMeta()).numberOfPatterns() > 0) {
                 //移除Pattern
                 ItemStack btnRemovePattern = new ItemBuilder(Material.BARRIER).amount(1).name(MessageUtil.format("&c" + tl("gui.remove-last-pattern"))).build();
-                menu.setItem(buttonPositionRemovePattern, btnRemovePattern);
+                setClickableItem(menu, 49, btnRemovePattern, ClickType.LEFT, new Clickable() {
+                    @Override
+                    public void action() {
+                        BannerMeta bm = (BannerMeta) currentBanner.getItemMeta();
+                        bm.removePattern(bm.numberOfPatterns() - 1);
+                        currentBanner.setItemMeta(bm);
+                        playerData.setCurrentEditBanner(currentBanner);
+                        InventoryMenuUtil.openMenu(player);
+                    }
+                });
             }
         }
         //開啟選單
         player.openInventory(menu);
-    }
-
-    @Override
-    public void onClick(InventoryClickEvent event) {
-        Player player = (Player) event.getWhoClicked();
-        PlayerData playerData = BannerMaker.getInstance().playerDataMap.get(player);
-        ItemStack itemStack = event.getCurrentItem();
-        //取得當前編輯中的旗幟
-        ItemStack currentBanner = playerData.getCurrentEditBanner();
-        int rawSlot = event.getRawSlot();
-        if (rawSlot >= 1 && rawSlot <= 17 && rawSlot % 9 != 0) {
-            if (currentBanner == null) {
-                //選擇底色
-                playerData.setCurrentEditBanner(itemStack);
-            } else {
-                //點擊顏色
-                playerData.setSelectedColor(DyeColorUtil.fromInt(itemStack.getDurability()));
-            }
-            InventoryMenuUtil.openMenu(player);
-            return;
-        }
-        if (rawSlot >= 19 && rawSlot <= 44 && rawSlot % 9 != 0) {
-            //新增Pattern
-            BannerMeta bm = (BannerMeta) itemStack.getItemMeta();
-            Pattern pattern = bm.getPattern(bm.numberOfPatterns() - 1);
-            BannerMeta currentBm = (BannerMeta) currentBanner.getItemMeta();
-            currentBm.addPattern(pattern);
-            currentBanner.setItemMeta(currentBm);
-            playerData.setCurrentEditBanner(currentBanner);
-            InventoryMenuUtil.openMenu(player);
-            return;
-        }
-        if (rawSlot < 45) {
-            return;
-        }
-        //修改狀態
-        if (rawSlot == buttonPositionMorePattern) {
-            playerData.setShowMorePatterns(!playerData.isShowMorePatterns());
-            InventoryMenuUtil.openMenu(player);
-            return;
-        }
-        if (rawSlot == buttonPositionRemovePattern) {
-            if (currentBanner.hasItemMeta() && ((BannerMeta) currentBanner.getItemMeta()).numberOfPatterns() > 0) {
-                BannerMeta bm = (BannerMeta) currentBanner.getItemMeta();
-                bm.removePattern(bm.numberOfPatterns() - 1);
-                currentBanner.setItemMeta(bm);
-                playerData.setCurrentEditBanner(currentBanner);
-                InventoryMenuUtil.openMenu(player);
-            }
-            return;
-        }
-        if (rawSlot == buttonPositionCreate) {
-            IOUtil.saveBanner(player, currentBanner);
-            playerData.setCurrentEditBanner(null);
-            InventoryMenuUtil.openMenu(player, InventoryMenuState.MAIN_MENU);
-            return;
-        }
-        if (rawSlot == buttonPositionDelete) {
-            playerData.setCurrentEditBanner(null);
-            InventoryMenuUtil.openMenu(player);
-            return;
-        }
-        if (rawSlot == buttonPositionBackToMenu) {
-            InventoryMenuUtil.openMenu(player, InventoryMenuState.MAIN_MENU);
-            return;
-        }
     }
 }
