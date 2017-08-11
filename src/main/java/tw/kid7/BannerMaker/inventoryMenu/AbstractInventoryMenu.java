09136f1cfe9d6cf325d2ca2e92a757e177178c5a
KID <jyhsu2000@yahoo.com.tw>
2017-08-12 00:29:37 +0800

    Create new ClickableItem system

--- src/main/java/tw/kid7/BannerMaker/inventoryMenu/AbstractInventoryMenu.java
+++ src/main/java/tw/kid7/BannerMaker/inventoryMenu/AbstractInventoryMenu.java
@@ -1,5 +1,34 @@
 package tw.kid7.BannerMaker.inventoryMenu;
 
-public abstract class AbstractInventoryMenu implements InventoryMenuInterface {
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
+public abstract class AbstractInventoryMenu implements InventoryMenuInterface {
+    final private Table<Integer, ClickType, Clickable> clickableItems = HashBasedTable.create();
+
+    @Override
+    public void onClick(InventoryClickEvent event) {
+        //點擊位置
+        int rawSlot = event.getRawSlot();
+        //點擊類型
+        ClickType clickType = event.getClick();
+        //找出對應動作
+        Clickable clickableItem = clickableItems.get(rawSlot, clickType);
+        if (clickableItem == null) {
+            return;
+        }
+        //執行動作
+        clickableItem.action();
+    }
+
+    final void setClickableItem(Inventory inventory, int slot, ItemStack itemStack, ClickType clickType, Clickable clickable) {
+        //放入點擊動作表
+        clickableItems.put(slot, clickType, clickable);
+        //顯示於選單
+        inventory.setItem(slot, itemStack);
+    }
 }
---
+++ src/main/java/tw/kid7/BannerMaker/inventoryMenu/Clickable.java
@@ -0,0 +1,8 @@
+package tw.kid7.BannerMaker.inventoryMenu;
+
+interface Clickable {
+    /**
+     * 點擊動作
+     */
+    void action();
+}
