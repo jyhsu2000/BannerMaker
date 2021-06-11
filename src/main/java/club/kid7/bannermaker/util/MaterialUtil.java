9db127aa158524ff3ae04c3a0637f53bc99c3643
KID <jyhsu2000@gmail.com>
2021-06-11 12:46:56 +0800

    Fix checking condition of MaterialUtil.isDye

--- src/main/java/club/kid7/bannermaker/util/MaterialUtil.java
+++ src/main/java/club/kid7/bannermaker/util/MaterialUtil.java
@@ -4,29 +4,7 @@ import org.bukkit.Material;
 
 public class MaterialUtil {
     public static boolean isDye(Material material) {
-        switch (material) {
-            //<editor-fold defaultstate="collapsed" desc="isDye">
-            case BONE_MEAL:
-            case ORANGE_DYE:
-            case MAGENTA_DYE:
-            case LIGHT_BLUE_DYE:
-            case YELLOW_DYE:
-            case LIME_DYE:
-            case PINK_DYE:
-            case GRAY_DYE:
-            case LIGHT_GRAY_DYE:
-            case CYAN_DYE:
-            case PURPLE_DYE:
-            case LAPIS_LAZULI:
-            case COCOA_BEANS:
-            case GREEN_DYE:
-            case RED_DYE:
-            case BLACK_DYE:
-                //</editor-fold>
-                return true;
-            default:
-                return false;
-        }
+        return material.name().contains("_DYE");
     }
 
     public static boolean isWool(Material material) {
